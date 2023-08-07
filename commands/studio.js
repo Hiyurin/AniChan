e4a57be8eb87ddbc1f941fd279de44a5f242b26f
Hiyurin <nguyenhuyentran03122004@gmail.com>
2023-08-07 17:10:30 +0700

    fix bug embed

--- commands/studio.js
+++ commands/studio.js
@@ -19,6 +19,7 @@ module.exports = {
             siteUrl
             media(isMain: true, sort: POPULARITY_DESC ) {
               nodes {
+                id
                 siteUrl
                 title {
                   romaji
@@ -46,30 +47,27 @@ module.exports = {
 
             console.log(`Thông tin studio: ${JSON.stringify(studioData)}`);
 
-            const embed = new MessageEmbed()
-                .setTitle(`Thông tin về studio: ${studioData.name}`)
-                .setURL(studioData.siteUrl)
-                .setDescription(`Danh sách các anime được sản xuất bởi studio ${studioData.name}:`)
-                .setTimestamp();
-
-            const animeRows = [];
-            for (const anime of studioData.media.nodes) {
+            const animeList = studioData.media.nodes.map((anime, index) => {
                 const animeTitle = anime.title.romaji;
                 const animeUrl = anime.siteUrl;
                 const animeYear = anime.startDate ? anime.startDate.year : 'N/A';
-                animeRows.push(`[${animeTitle}](${animeUrl}) - Năm sản xuất: ${animeYear}`);
-            }
+                return `${index + 1}. [${animeTitle}](${animeUrl}) - Năm sản xuất: ${animeYear}`;
+            }).join('\n');
 
-            const pageSize = 5;
-            const totalPages = Math.ceil(animeRows.length / pageSize);
+            const pageSize = 10;
+            const totalPages = Math.ceil(studioData.media.nodes.length / pageSize);
             let currentPage = 0;
 
             const updateEmbed = () => {
                 const startIdx = currentPage * pageSize;
                 const endIdx = startIdx + pageSize;
-                const animeList = animeRows.slice(startIdx, endIdx).join('\n');
+                const displayedAnime = animeList.split('\n').slice(startIdx, endIdx).join('\n');
 
-                embed.setDescription(`Danh sách các anime được sản xuất bởi studio ${studioData.name}:\n${animeList}`);
+                const embed = new MessageEmbed()
+                    .setTitle(`Thông tin về studio: ${studioData.name}`)
+                    .setURL(studioData.siteUrl)
+                    .setDescription(`Danh sách các anime được sản xuất bởi studio ${studioData.name}:\n${displayedAnime}`)
+                    .setTimestamp();
 
                 const row = new MessageActionRow()
                     .addComponents(
@@ -91,7 +89,7 @@ module.exports = {
             const interactionReply = await interaction.reply(updateEmbed());
 
             const filter = i => i.customId === 'prev' || i.customId === 'next';
-            const collector = interactionReply.createMessageComponentCollector({ filter, time: 30000 });
+            const collector = interaction.channel.createMessageComponentCollector({ filter, time: 30000 });
 
             collector.on('collect', async i => {
                 if (i.customId === 'prev' && currentPage > 0) {
@@ -99,11 +97,11 @@ module.exports = {
                 } else if (i.customId === 'next' && currentPage < totalPages - 1) {
                     currentPage++;
                 }
-                await i.update(updateEmbed());
+                await interaction.editReply(updateEmbed());
             });
 
             collector.on('end', collected => {
-                interactionReply.edit({ components: [] });
+                interaction.editReply({ components: [] });
             });
         } catch (error) {
             console.error('Lỗi khi lấy thông tin studio:', error);
