dbb68a5f8b02a020150b8fd71737aadd31c45f34
Kizyn <121009623+kizynm@users.noreply.github.com>
2023-08-13 20:19:54 +0700

    Add image upscale using waifu2x

---
+++ commands/upscale.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const axios = require('axios');
+const fs = require('fs');
+const waifu2x = require('waifu2x');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('upscale')
+    .setDescription('Tăng chất lượng hình ảnh bằng waifu2x')
+    .addStringOption(option => option.setName('image').setDescription('Liên kết đến hình ảnh cần upscale').setRequired(true)),
+  async execute(interaction) {
+    const imageUrl = interaction.options.getString('image');
+
+    try {
+      const response = await axios.get(imageUrl, {
+        responseType: 'arraybuffer',
+      });
+
+      const imageBuffer = Buffer.from(response.data, 'binary');
+
+      const waifu2xResult = await waifu2x(imageBuffer, { noise: 3, scale: 2 });
+
+      const tempFilePath = 'temp.png';
+      fs.writeFileSync(tempFilePath, waifu2xResult);
+
+      interaction.reply({ files: [tempFilePath] });
+
+      fs.unlinkSync(tempFilePath);
+    } catch (error) {
+      console.error('Lỗi khi upscale hình ảnh:', error);
+      interaction.reply('Đã xảy ra lỗi khi upscale hình ảnh.');
+    }
+  },
+};
