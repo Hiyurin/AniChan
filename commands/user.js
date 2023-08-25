ae5735da07f2e9027c93bdb0e39770e862d8787b
Kizyn <121009623+kizynm@users.noreply.github.com>
2023-08-25 11:50:53 +0700

    Update user.js

--- commands/user.js
+++ commands/user.js
@@ -5,61 +5,81 @@ const axios = require('axios');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('user')
-    .setDescription('Hiển thị thông tin tổng quan về người dùng.')
+    .setDescription('Lấy thông tin cá nhân của người dùng trên AniList.')
     .addStringOption(option => option.setName('username').setDescription('Tên người dùng trên AniList').setRequired(true)),
   async execute(interaction) {
     const username = interaction.options.getString('username');
 
     try {
-      const response = await axios.post('https://graphql.anilist.co', {
-        query: `
-          query ($username: String) {
-            User (name: $username) {
-              id
-              name
-              about
-              avatar {
-                large
-              }
-              statistics {
-                anime {
+      const query = `
+        query ($username: String) {
+          User(name: $username) {
+            id
+            name
+            about
+            avatar {
+              large
+            }
+            statistics {
+              anime {
                 count
-                  minutesWatched
-                }
-                manga {
-                  count
-                  chaptersRead
-                }
+                minutesWatched
+              }
+              manga {
+                count
+                chaptersRead
               }
             }
           }
-        `,
+        }
+      `;
+
+      const response = await axios.post('https://graphql.anilist.co', {
+        query,
         variables: { username },
       });
 
-      const user = response.data.data.User;
+      const userData = response.data.data.User;
 
-      if (!user) {
-        console.log(`Không tìm thấy người dùng: ${username}`);
-        return interaction.reply('Không tìm thấy người dùng.');
+      if (!userData) {
+        console.log(`Không tìm thấy thông tin người dùng: ${username}`);
+        return interaction.reply(`Không tìm thấy thông tin người dùng: **${username}**`);
       }
 
-      console.log(`Thông tin người dùng: ${JSON.stringify(user)}`);
+      console.log(`Thông tin người dùng: ${JSON.stringify(userData)}`);
 
       const embed = new MessageEmbed()
-        .setTitle(`${user.name}'s Profile`)
-        .setDescription(user.about || 'Không có thông tin.')
-        .setThumbnail(user.avatar?.large || '')
-        .addField('Số lượng anime đã xem', `${user.statistics.anime?.count || 0} bộ`, true)
-        .addField('Thời gian đã xem', `${user.statistics.anime?.minutesWatched || 0} phút`, true)
-        .addField(' Số bộ manga đã đọc', `${user.statistics.manga?.count || 0} chương`, true)
-        .addField(' Số chương manga đã đọc', `${user.statistics.manga?.chaptersRead || 0} chương`, true)
+        .setTitle(userData.name)
+        .setColor('#C6FFFF')
+        .addFields(
+          {
+            name: 'Số lượng anime đã xem',
+            value: `${userData.statistics.anime.count} bộ.`,
+            inline: true,
+          },
+          {
+            name: 'Thời gian anime đã xem',
+            value: `${userData.statistics.anime.minutesWatched} phút.`,
+            inline: true,
+          },
+          {
+            name: 'Số lượng manga đã đọc',
+            value: `${userData.statistics.manga.count} bộ.`,
+            inline: true,
+          },
+          {
+            name: 'Số lượng chương manga đã đọc',
+            value: `${userData.statistics.manga.chaptersRead} chương.`,
+            inline: true,
+          }
+        )
+        .setThumbnail(userData.avatar.large)
         .setTimestamp();
 
       interaction.reply({ embeds: [embed] });
     } catch (error) {
-      console.error('Lỗi khi tìm kiếm người dùng:', error);
-      interaction.reply('Đã xảy ra lỗi khi tìm kiếm người dùng.');
+      console.error('Lỗi khi lấy thông tin người dùng:', error);
+      interaction.reply('Đã xảy ra lỗi khi lấy thông tin người dùng.');
     }
   },
 };
